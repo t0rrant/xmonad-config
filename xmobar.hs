2e3ba67573ed7b7c54c681fe8084abb837c7b6a5
Manuel Torrinha <manuel.torrinha@ist.utl.pt>
2013-10-14 18:03:38 +0100

    Changed xmobar position to top and date is printed differently

--- xmobar.hs
+++ xmobar.hs
@@ -5,13 +5,13 @@
 Config { font= "xft:Bitstream Vera Sans Mono:size=9:antialias=true"
        , bgColor = "black"
        , fgColor = "grey"
-       , position = BottomW L 100
+       , position = TopW L 95
        , lowerOnStart = True
        , commands = [ Run Weather "LPPT" ["-t","Lx: <tempC>C <skyCondition>","-L","7","-H","30","--normal","green","--high","red","--low","lightblue"] 36000
 --                    , Run Network "eth0" ["-t","<dev>: <rx> | <tx>","-L","10","-H","200","--normal","green","--high","red","-S","True"] 10
                     , Run MultiCpu ["-t", "Cpu: <total0> <total1> <total2> <total3>","-L","3","-H","70","--low","yellow","--normal","green","--high","red","-w", "3"] 10
                     , Run Memory ["-t","Mem: <usedratio>%","-L","3","-H","90","--normal","green","--high","red"] 10
-                    , Run Date "%a %b %_d" "date" 10
+                    , Run Date "%a %b %_d %H:%M" "date" 10
                     , Run StdinReader
                     ]
        , sepChar = "%"
